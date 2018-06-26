3fd113b5b06c17da5a16e94b322f4aec68046064
Extomine <40575604+Extomine@users.noreply.github.com>
2018-06-25 20:04:40 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('lol carlos is fat ok made by extomine');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong ur mom hahahalolllool');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
